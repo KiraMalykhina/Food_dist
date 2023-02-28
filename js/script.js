437335180f41c92d6a63cb869e598ca7e6a100d8
Malykhina Kira <kira4ka12@gmail.com>
2023-02-28 16:25:42 +0200

    Aded new file: .gitignore, db.json, package-lock.json, package.json

--- js/script.js
+++ js/script.js
@@ -350,4 +350,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    fetch('http://localhost:3000/menu')
+         .then(data => data.json())
+         .then(res =>console.log(res));
+
 });
